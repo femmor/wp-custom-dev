d3f84e460875e93715b3ddca82464953e42d744c
Emmanuel <fegomson@gmail.com>
2020-12-01 12:40:26 -0600

    adding menu support

--- wp-content/themes/fem/functions.php
+++ wp-content/themes/fem/functions.php
@@ -5,9 +5,11 @@ define('FM_DEV_MODE', true);
 
 // Includes
 include(get_theme_file_path( './includes/front/enqueue.php' ));
+include(get_theme_file_path( './includes/setup.php' ));
 
 // Hooks and Actions
 add_action( 'wp_enqueue_scripts', "fm_enqueue" );
+add_action( 'after_setup_theme', 'fm_setup_theme');
 
 // Shortcodes
 
